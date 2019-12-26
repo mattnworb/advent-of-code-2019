ea1e04411f24049224011fd1067a9e62f082c526
Matt Brown <mattbrown@spotify.com>
2019-12-25 21:48:58 -0500

    done with problem 07 part 2

--- problem07.py
+++ problem07.py
@@ -4,7 +4,13 @@ if __name__ == "__main__":
     with open("problem07/input", "r") as f:
         my_input = f.readlines()
         my_input = "".join(my_input)
-    program = problem07.main.parse_program(my_input)
-    max_val, phases = problem07.main.find_max_thruster(program)
 
+    program = problem07.main.parse_program(my_input)
+
+    # part 1
+    max_val, phases = problem07.main.find_max_thruster(program)
+    print(max_val)
+
+    # part 2
+    max_val, phases = problem07.main.find_max_thruster(program, feedback_mode=True)
     print(max_val)
