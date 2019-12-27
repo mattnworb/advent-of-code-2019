df030aca0dc606edf8e35f65adcd41fdd6942c85
Matt Brown <mattbrown@spotify.com>
2019-12-27 14:58:58 -0500

    Problem 09: Part 2

--- problem09.py
+++ problem09.py
@@ -20,3 +20,21 @@ if __name__ == "__main__":
     assert result == computer.RunResult.HALTED
     print("Part 1:")
     print(outputs)
+
+    # Finally, you can lock on to the Ceres distress signal! You just need to
+    # boost your sensors using the BOOST program.
+    #
+    # The program runs in sensor boost mode by providing the input instruction
+    # the value 2. Once run, it will boost the sensors automatically, but it
+    # might take a few seconds to complete the operation on slower hardware. In
+    # sensor boost mode, the program will output a single value: the coordinates
+    # of the distress signal.
+    #
+    # Run the BOOST program in sensor boost mode. What are the coordinates of the
+    # distress signal?
+    c = computer.Computer(program, inputs=2, verbose=False)
+    outputs, result = c.run()
+
+    assert result == computer.RunResult.HALTED
+    print("Part 2:")
+    print(outputs)
