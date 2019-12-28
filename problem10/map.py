b3176505bd743c30b006d7a179795c25912ee860
Matt Brown <mattbrown@spotify.com>
2019-12-27 21:18:05 -0500

    problem 10 part 1: all tests passing

--- problem10/map.py
+++ problem10/map.py
@@ -12,13 +12,20 @@ def slope(a, b):
     i.e. returns (2,1) instead of (4,2).
     """
     s = b[0] - a[0], b[1] - a[1]
-    return s[0] // gcd(s[0], s[1]), s[1] // gcd(s[0], s[1])
+    # take the absolute value of the greatest common divisor
+    # otherwise for negative slopes we'll end up dividing negative by negative
+    div = abs(gcd(s[0], s[1]))
+    return s[0] // div, s[1] // div
 
 
 def gcd(a, b):
     if b == 0:
         return a
     return gcd(b, a % b)
+
+
+def add(a, b):
+    return a[0] + b[0], a[1] + b[1]
 
 
 class AsteroidMap:
@@ -35,6 +42,8 @@ class AsteroidMap:
         # test that the two dimensional array has the same length in each row
         assert len(set([len(row) for row in positions])) == 1
         self.positions = positions
+        self.num_x = len(positions[0])
+        self.num_y = len(positions)
 
     def num_asteroids(self):
         """Returns the number of asteroids in the map."""
@@ -50,9 +59,56 @@ class AsteroidMap:
                 if pos == 1:
                     yield (x, y)
 
+    def is_asteroid(self, pos):
+        x, y = pos
+        # these are reversed
+        return self.positions[y][x] == 1
+
+    def find_best_monitoring_station(self):
+        best_asteroid = None
+        highest_count = 0
+        for asteroid in self.asteroid_positions():
+            count = self.count_line_of_sight(asteroid)
+            if count > highest_count:
+                highest_count = count
+                best_asteroid = asteroid
+        return best_asteroid
+
     def count_line_of_sight(self, asteroid):
         """Count how many asteroids are in line of sight from the given asteroid (with position (x,y))."""
+        count = 0
+
         for other in self.asteroid_positions():
             if other == asteroid:
                 continue
-            # compute the slope/"vector" between these two asteroids
+
+            if self.is_in_line_of_sight(asteroid, other):
+                count += 1
+
+        return count
+
+    def in_bounds(self, pos) -> bool:
+        """Test if position pos is in the map."""
+        return (
+            pos[0] >= 0 and pos[0] < self.num_x and pos[1] >= 0 and pos[1] < self.num_y
+        )
+
+    def is_in_line_of_sight(self, asteroid1, asteroid2) -> bool:
+        """Test if asteroid2 is in line of sight from asteroid1"""
+
+        if not self.is_asteroid(asteroid1) or not self.is_asteroid(asteroid2):
+            return False
+
+        s = slope(asteroid1, asteroid2)
+
+        candidate = add(asteroid1, s)
+        # take multiples of s
+        while self.in_bounds(candidate):
+            if candidate == asteroid2:
+                return True
+
+            if self.is_asteroid(candidate):
+                # found a blocker
+                return False
+
+            candidate = add(candidate, s)
--- problem10/test_map.py
+++ problem10/test_map.py
@@ -23,6 +23,32 @@ map2 = """
 """
 
 map3 = """
+#.#...#.#.
+.###....#.
+.#....#...
+##.#.#.#.#
+....#.#.#.
+.##..###.#
+..#...##..
+..##....##
+......#...
+.####.###.
+"""
+
+map4 = """
+.#..#..###
+####.###.#
+....###.#.
+..###.##.#
+##.##.#.#.
+....###..#
+..#.#..#.#
+#..#.#.###
+.##...##.#
+.....#.#..
+"""
+
+map5 = """
 .#..##.###...#######
 ##.############..##.
 .#.######.########.#
@@ -47,7 +73,7 @@ map3 = """
 
 
 class TestAsteroidMap:
-    @pytest.mark.parametrize("test_map", [map1, map2, map3])
+    @pytest.mark.parametrize("test_map", [map1, map2, map3, map4, map5])
     def test_num_asteroids(self, test_map):
         assert test_map.count("#") == AsteroidMap.parse(test_map).num_asteroids()
 
@@ -69,6 +95,50 @@ class TestAsteroidMap:
         }
         assert expected == actual
 
+    def test_is_asteroid(self):
+        m1 = AsteroidMap.parse(map1)
+        assert not m1.is_asteroid((0, 0))
+        assert m1.is_asteroid((1, 0))
+        assert m1.is_asteroid((4, 4))
+
+    def test_is_in_line_of_sight(self):
+        m1 = AsteroidMap.parse(map1)
+        assert m1.is_in_line_of_sight((1, 0), (4, 0))
+        # same but order flopped
+        assert m1.is_in_line_of_sight((4, 0), (1, 0))
+
+        assert m1.is_in_line_of_sight((1, 0), (0, 2))
+        assert m1.is_in_line_of_sight((1, 0), (1, 2))
+
+        assert not m1.is_in_line_of_sight((1, 0), (3, 4))  # blocked by (2, 2)
+        assert not m1.is_in_line_of_sight((1, 0), (4, 1))  # not an asteroid
+
+    def test_count_line_of_sight(self):
+        """
+        .7..7
+        .....
+        67775
+        ....7
+        ...87
+        """
+        m1 = AsteroidMap.parse(map1)
+        assert 7 == m1.count_line_of_sight((1, 0))
+        assert 7 == m1.count_line_of_sight((4, 0))
+
+    @pytest.mark.parametrize(
+        "test_map,expected",
+        [
+            (map1, (3, 4)),
+            (map2, (5, 8)),
+            (map3, (1, 2)),
+            (map4, (6, 3)),
+            (map5, (11, 13)),
+        ],
+    )
+    def test_find_best_monitoring_station(self, test_map, expected):
+        m = AsteroidMap.parse(test_map)
+        assert expected == m.find_best_monitoring_station()
+
 
 def test_slope():
     assert slope((1, 2), (3, 3)) == (2, 1)
@@ -79,3 +149,8 @@ def test_slope():
 
     # same line
     assert slope((1, 2), (5, 2)) == (1, 0)
+
+    assert slope((1, 0), (4, 0)) == (1, 0)
+
+    # order swapped should have negative slope
+    assert slope((4, 0), (1, 0)) == (-1, 0)
