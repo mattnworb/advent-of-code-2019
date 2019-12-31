2939e634201ebbf308abcdc6c4ed8afe917dc10c
Matt Brown <mattbrown@spotify.com>
2019-12-31 13:01:45 -0500

    test cases for problem 12 part 1 passing

--- problem12/moon.py
+++ problem12/moon.py
@@ -1,17 +1,92 @@
-class Position:
-    def __init__(self, x: int, y: int, z: int):
-        self.x = x
-        self.y = y
-        self.z = z
+from typing import Tuple, List, Iterable
+from itertools import combinations
+import logging
 
-    def __repr__(self):
-        return f"x={self.x: d}, y={self.y: d}, z={self.z: d}"
+logger = logging.getLogger(__name__)
+
+Tuple3 = Tuple[int, int, int]
+
+
+# class Position:
+#     def __init__(self, x: int, y: int, z: int):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+
+#     def __repr__(self):
+#         return f"x={self.x: d}, y={self.y: d}, z={self.z: d}"
 
 
 class Moon:
-    def __init__(self, position: Position, velocity: Position):
+    def __init__(self, position: Tuple3, velocity=(0, 0, 0)):
         self.position = position
         self.velocity = velocity
 
     def __repr__(self):
         return f"pos=<{self.position}>, vel=<{self.velocity}>"
+
+    def add_velocity_to_position(self):
+        for axis in [0, 1, 2]:
+            self.position = _add(self.position, axis, self.velocity[axis])
+
+
+def _add(t: Tuple3, axis: int, val: int) -> Tuple3:
+    """Adds one to the value in the given position and returns new tuple"""
+    if axis == 0:
+        return t[0] + val, t[1], t[2]
+    if axis == 1:
+        return t[0], t[1] + val, t[2]
+    if axis == 2:
+        return t[0], t[1], t[2] + val
+    raise ValueError()
+
+
+def _copy_moons(moons):
+    return [Moon(m.position, m.velocity) for m in moons]
+
+
+def run_simulation(moons: List[Moon], rounds: int) -> List[Moon]:
+    moons = _copy_moons(moons)
+
+    # Simulate the motion of the moons in time steps. Within each time step,
+    # first update the velocity of every moon by applying gravity. Then, once
+    # all moons' velocities have been updated, update the position of every moon
+    # by applying velocity. Time progresses by one step once all of the
+    # positions are updated.
+
+    for round in range(rounds):
+        moons = apply_gravity(moons)
+        logger.debug("Round=%d after gravity, moons=%s", round, moons)
+
+        # Once all gravity has been applied, apply velocity: simply add the
+        # velocity of each moon to its own position. For example, if Europa has
+        # a position of x=1, y=2, z=3 and a velocity of x=-2, y=0,z=3, then its
+        # new position would be x=-1, y=2, z=6. This process does not modify the
+        # velocity of any moon.
+        for moon in moons:
+            moon.add_velocity_to_position()
+
+    return moons
+
+
+def apply_gravity(moons: List[Moon]) -> List[Moon]:
+    # To apply gravity, consider every pair of moons. On each axis (x, y, and
+    # z), the velocity of each moon changes by exactly +1 or -1 to pull the
+    # moons together. For example, if Ganymede has an x position of 3, and
+    # Callisto has a x position of 5, then Ganymede's x velocity changes by +1
+    # (because 5 > 3) and Callisto's x velocity changes by -1 (because 3 < 5).
+    # However, if the positions on a given axis are the same, the velocity on
+    # that axis does not change for that pair of moons.
+
+    new_moons = [Moon(m.position, m.velocity) for m in moons]
+
+    for m1, m2 in combinations(new_moons, 2):
+
+        for axis in [0, 1, 2]:
+            if m1.position[axis] < m2.position[axis]:
+                m1.velocity = _add(m1.velocity, axis, 1)
+                m2.velocity = _add(m2.velocity, axis, -1)
+            elif m1.position[axis] > m2.position[axis]:
+                m1.velocity = _add(m1.velocity, axis, -1)
+                m2.velocity = _add(m2.velocity, axis, 1)
+    return new_moons
--- problem12/test_moon.py
+++ problem12/test_moon.py
@@ -1,9 +1,129 @@
-from .moon import Moon, Position
+from collections import namedtuple
+import logging
+from sys import stdout
+
+logging.basicConfig(level=logging.INFO)
+
+import pytest  # type: ignore
+
+import problem12.moon as moon
 
 
 class TestMoon:
-    def test_repr(self):
-        pos = Position(-1, 0, 2)
-        vel = Position(0, 0, 0)
-        m = Moon(pos, vel)
-        assert repr(m) == "pos=<x=-1, y= 0, z= 2>, vel=<x= 0, y= 0, z= 0>"
+    def test_velocity_defaults_to_zero(self):
+        m = moon.Moon((1, 1, 1))
+        assert m.velocity == (0, 0, 0)
+
+    def test_add_velocity_to_position(self):
+        m = moon.Moon((1, 2, 3), (-1, 4, 10))
+        m.add_velocity_to_position()
+        assert m.position == (0, 6, 13)
+
+
+#     def test_repr(self):
+#         pos = Position(-1, 0, 2)
+#         vel = Position(0, 0, 0)
+#         m = Moon(pos, vel)
+#         assert repr(m) == "pos=<x=-1, y= 0, z= 2>, vel=<x= 0, y= 0, z= 0>"
+
+Expected = namedtuple("Expected", ["pos", "vel"])
+
+
+def test_run_simulation_zero_rounds():
+    moons = [
+        moon.Moon((-1, 0, 2)),
+        moon.Moon((2, -10, -7)),
+        moon.Moon((4, -8, 8)),
+        moon.Moon((3, 5, -1)),
+    ]
+
+    moon.run_simulation(moons, 0)
+    assert moons[0].velocity == (0, 0, 0)
+    assert moons[1].velocity == (0, 0, 0)
+    assert moons[2].velocity == (0, 0, 0)
+    assert moons[3].velocity == (0, 0, 0)
+    assert moons[0].position == (-1, 0, 2)
+    assert moons[1].position == (2, -10, -7)
+    assert moons[2].position == (4, -8, 8)
+    assert moons[3].position == (3, 5, -1)
+
+
+def test_apply_gravity():
+    # For example, if Ganymede has an x position of 3, and Callisto has a x
+    # position of 5, then Ganymede's x velocity changes by +1 (because 5 > 3)
+    # and Callisto's x velocity changes by -1 (because 3 < 5).
+    moons = [moon.Moon((3, 3, 3)), moon.Moon((5, 5, 5))]
+    after = moon.apply_gravity(moons)
+    assert len(after) == len(moons)
+    assert after[0].velocity == (1, 1, 1)
+    assert after[1].velocity == (-1, -1, -1)
+
+
+def test_apply_gravity_same_position():
+    # However, if the positions on a given axis are the same, the velocity on
+    # that axis does not change for that pair of moons.
+    moons = [moon.Moon((5, 5, 5)), moon.Moon((5, 5, 5))]
+    after = moon.apply_gravity(moons)
+    assert len(after) == len(moons)
+    assert after[0].velocity == (0, 0, 0)
+    assert after[1].velocity == (0, 0, 0)
+
+
+EXAMPLE_1_MOONS = [
+    moon.Moon((-1, 0, 2)),
+    moon.Moon((2, -10, -7)),
+    moon.Moon((4, -8, 8)),
+    moon.Moon((3, 5, -1)),
+]
+
+EXAMPLE_2_MOONS = [
+    moon.Moon((-8, -10, 0)),
+    moon.Moon((5, 5, 10)),
+    moon.Moon((2, -7, 3)),
+    moon.Moon((9, -8, -3)),
+]
+
+
+@pytest.mark.parametrize(
+    "num_rounds,input_moons,expected_moons",
+    [
+        (
+            1,
+            EXAMPLE_1_MOONS,
+            [
+                Expected(pos=(2, -1, 1), vel=(3, -1, -1)),
+                Expected(pos=(3, -7, -4), vel=(1, 3, 3)),
+                Expected(pos=(1, -7, 5), vel=(-3, 1, -3)),
+                Expected(pos=(2, 2, 0), vel=(-1, -3, 1)),
+            ],
+        ),
+        (
+            10,
+            EXAMPLE_1_MOONS,
+            [
+                Expected(pos=(2, 1, -3), vel=(-3, -2, 1)),
+                Expected(pos=(1, -8, 0), vel=(-1, 1, 3)),
+                Expected(pos=(3, -6, 1), vel=(3, 2, -3)),
+                Expected(pos=(2, 0, 4), vel=(1, -1, -1)),
+            ],
+        ),
+        (
+            100,
+            EXAMPLE_2_MOONS,
+            [
+                Expected(pos=(8, -12, -9), vel=(-7, 3, 0)),
+                Expected(pos=(13, 16, -3), vel=(3, -11, -5)),
+                Expected(pos=(-29, -11, -1), vel=(-3, 7, 4)),
+                Expected(pos=(16, -13, 23), vel=(7, 1, 1)),
+            ],
+        ),
+    ],
+)
+def test_run_simulation(num_rounds, input_moons, expected_moons):
+    actual = moon.run_simulation(list(input_moons), num_rounds)
+    assert len(actual) == len(input_moons)
+
+    for n, expected in enumerate(expected_moons):
+        print("Expected=", expected, ", Actual=", actual[n])
+        assert expected.pos == actual[n].position
+        assert expected.vel == actual[n].velocity
